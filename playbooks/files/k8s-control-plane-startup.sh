dd2e7f354d19b1494a02d927a2dd88e4b0bcd30a
HubbeKing <github@hubbe.club>
2025-01-11 15:18:34 +0200

    Wait for all apiservers to be healthy on startup?

--- playbooks/files/k8s-control-plane-startup.sh
+++ playbooks/files/k8s-control-plane-startup.sh
@@ -2,7 +2,12 @@
 
 KUBECONFIG=/etc/kubernetes/admin.conf
 
-# assume we're after a UPS shutdown, these commands are harmless if we aren't anyway
+# assume we're after a UPS shutdown
+# wait for kube-apiservers to be ready
+# TODO: how to just wait for healthy apiserver instead?
+# kubectl get --raw='/readyz' ??
+sudo kubectl -n kube-system wait --for=condition=Ready --all --timeout=120s pod/kube-apiserver-pacifica pod/kube-apiserver-pegasus pod/kube-apiserver-valkyrie
+
 # scale up mariadb
 sudo kubectl -n database scale statefulset mariadb --replicas 3
 # scale up openldap
