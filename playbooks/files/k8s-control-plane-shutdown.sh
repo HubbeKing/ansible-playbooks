0151cacfe1f68d076aea70aee9b745a75f27c474
HubbeKing <github@hubbe.club>
2025-01-24 08:36:33 +0200

    Update startup/shutdown script to reflect mariadb/ldap changes

--- playbooks/files/k8s-control-plane-shutdown.sh
+++ playbooks/files/k8s-control-plane-shutdown.sh
@@ -5,8 +5,6 @@ KUBECONFIG=/etc/kubernetes/admin.conf
 # this script is run as root through upsmon, no need for sudo
 # scale down mariadb
 kubectl -n database scale statefulset mariadb --replicas 0
-# scale down openldap
-kubectl -n authelia scale statefulset openldap --replicas 0
 # wait until mariadb-0 is deleted
 kubectl -n database wait --for=delete pod/mariadb-0 --timeout=120s
 
--- playbooks/files/k8s-control-plane-startup.sh
+++ playbooks/files/k8s-control-plane-startup.sh
@@ -9,6 +9,4 @@ KUBECONFIG=/etc/kubernetes/admin.conf
 sudo kubectl -n kube-system wait --for=condition=Ready --all --timeout=120s pod/kube-apiserver-pacifica pod/kube-apiserver-pegasus pod/kube-apiserver-valkyrie
 
 # scale up mariadb
-sudo kubectl -n database scale statefulset mariadb --replicas 3
-# scale up openldap
-sudo kubectl -n authelia scale statefulset openldap --replicas 3
+sudo kubectl -n database scale statefulset mariadb --replicas 5
